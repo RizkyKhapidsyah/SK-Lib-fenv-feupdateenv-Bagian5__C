bddcc9f8f6cc7f7cc5d92289d560c6ca3d0a9ae3
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-05-01 11:35:28 +0700

    Add project files.

---
+++ SK-Lib-fenv-feupdateenv-Bagian5__C/Source.c
@@ -0,0 +1,74 @@
+/* Contoh Program Dalam Bahasa C */
+
+#include <stdio.h>
+#include <fenv.h>
+#include <float.h>
+#include <conio.h>
+
+#pragma fenv_access (on)
+
+/*  Source by cppreference
+    Modified For Learn by Rizky Khapidsyah 
+    I.D.E : Visual Studio 2019
+    Compiler : MVC (Microsoft)
+*/
+
+void show_fe_exceptions(void) {
+    printf("current exceptions raised : ");
+
+    if (fetestexcept(FE_DIVBYZERO)) {
+        printf(" FE_DIVBYZERO");
+    }
+
+    if (fetestexcept(FE_INEXACT)) {
+        printf(" FE_INEXACT");
+    }
+    
+    if (fetestexcept(FE_INVALID)){
+        printf(" FE_INVALID");
+    }
+    
+    if (fetestexcept(FE_OVERFLOW)) {
+        printf(" FE_OVERFLOW");
+    }
+
+    if (fetestexcept(FE_UNDERFLOW)) {
+        printf(" FE_UNDERFLOW");
+    }
+
+    if (fetestexcept(FE_ALL_EXCEPT) == 0) {
+        printf(" none");
+    }
+
+    printf("\n");
+}
+
+double x2(double x)   /* times two */ {
+    fenv_t curr_excepts;
+
+    /* Save and clear current f-p environment. */
+    feholdexcept(&curr_excepts);
+
+    /* Raise inexact and overflow exceptions. */
+    printf("In x2():  x = %f\n", x = x * 2.0);
+    show_fe_exceptions();
+    feclearexcept(FE_INEXACT);   /* hide inexact exception from caller */
+
+    /* Merge caller's exceptions (FE_INVALID)        */
+    /* with remaining x2's exceptions (FE_OVERFLOW). */
+    feupdateenv(&curr_excepts);
+    return x;
+}
+
+int main(void) {
+    feclearexcept(FE_ALL_EXCEPT);
+    feraiseexcept(FE_INVALID);   /* some computation with invalid argument */
+    show_fe_exceptions();
+
+    printf("x2(DBL_MAX) = %f\n", x2(DBL_MAX));
+    
+    show_fe_exceptions();
+
+    _getch();
+    return 0;
+}
